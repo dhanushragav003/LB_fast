bace41f70388cb21b00b2b884a270d5040ea7286
dhanushragav003 <dhanushragav003@gmail.com>
2026-02-15 21:13:24 +0530

    feat: enhance build_cron_expression to support time zone conversion for scheduling

--- app/helpers/tasks.py
+++ app/helpers/tasks.py
@@ -1,19 +1,29 @@
-def build_cron_expression(time_str, frequency, time_zone=None):
-    # time_str format: "HH:MM" or "HH:MM:SS"
-    parts = time_str.split(":")
-    
-    # ensure we have HH and MM
-    hour = parts[0]
-    minute = parts[1]
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-    # base: minute hour dom month dow
+def convert_to_utc(hour: int, minute: int, time_zone: str):
+    now_local = datetime.now(ZoneInfo(time_zone))
+
+    local = now_local.replace(
+        hour=hour,
+        minute=minute,
+        second=0,
+        microsecond=0
+    )
+    scheduled_utc = local.astimezone(ZoneInfo("UTC"))
+    return scheduled_utc.hour, scheduled_utc.minute
+
+def build_cron_expression(time_str, frequency, time_zone=None):
+    parts = time_str.split(":")
+    hour = parts[0]
+    minute = parts[1]-1
+    if time_zone:
+        hour , minute  = convert_to_utc(hour,minute,time_zone)
     if frequency == "everyday":
         cron = f"{minute} {hour} * * *"
     elif frequency == "everyweekday":
         cron = f"{minute} {hour} * * 1-5"
     else:
         raise ValueError("Unsupported frequency")
-
-    if time_zone:
-        cron = f"CRON_TZ={time_zone} {cron}"
     return cron
+
